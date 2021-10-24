b0da287857b6732abf2f79de6d27fec303ab9ef4
madhankumar2211 <madhankumar199722@gmail.com>
2021-10-24 12:56:53 +0530

    initial

--- src/app/tracking/tracking.component.ts
+++ src/app/tracking/tracking.component.ts
@@ -17,49 +17,30 @@ export class TrackingComponent implements OnInit {
   bdate:any;
   ddate : any;
   bar:any;
-
-  // constructor(public iS:InformService,public http:HttpClient) { }
   alldatas:any
+  alldata: FormGroup;
+  submitted : any;
 
+  constructor(private fb: FormBuilder,public pS: ProfileService,public http: HttpClient,public router : Router) {}  
   
-  ngOnInit(): void{}
-   /* {
-      this.iS.getList().subscribe((data) => {
-        this.alldatas = data;
-      });
-    },public iS: InformService,public http: HttpClient
-   }*/
-  
-  alldata: FormGroup = new FormGroup({});  
-   constructor(private fb: FormBuilder,public pS: ProfileService,public http: HttpClient,public router : Router) {  
-    this.alldata = fb.group({  
-      nam: ['', [Validators.required]],
-      // mob: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]] ,
-      tid: ['', [Validators.required,Validators.pattern("[A-Z]{3}[0-9]{3}")]]
-      // bd: ['',[Validators.required]]
+  ngOnInit(): void{
+    this.alldata = this.fb.group({  
+      tid: ['', Validators.required]
     })  
-  }  
-get f(){  
-    return this.alldata.controls;  
+  }
+  
+  get fid(){  
+    return this.alldata.controls['tid'];  
   }  
   getall(): void
   {
-    //console.log(this.alldata);
-    
-      // this.router.navigateByUrl('/second')
+    this.submitted = true;
       this.pS.getListOne(this.alldata.value).subscribe((data) => {
-        //console.log(data.order_status);
-        //console.log("hii");
-        
-        
         this.track = data["_id"];
         this.bdate = data.order_status["b_date"];
         this.status = data.order_status["status"];
         this.phone = data.order_status["phone"];
         this.ddate = data.order_status["d_date"];
-        
-        //console.log(data['tid']);
-
         if(this.status == "Start"){
           this.bar = 32;
         }
@@ -69,15 +50,9 @@ get f(){
         }
         else{
           this.bar = 100;
-
         }
-        
       })
-   
-
   }
-  
-
 }
   
 
